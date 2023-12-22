99555b27c3167f7de9d9aad4a5bd748111ca316c
danny <danny.nothdurft@sugarshape.de>
2023-12-22 12:46:48 +0100

    Logo

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,7 +1,7 @@
 import { Plus } from "lucide-react";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 
-//import { Logo } from "@/components/logo";
+import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
 import { FormPopover } from "@/components/form/form-popover";
 
@@ -12,7 +12,9 @@ export const Navbar = () => {
     <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
       <MobileSidebar />
       <div className="flex items-center gap-x-4">
-        <div className="hidden md:flex">{/* <Logo /> */}</div>
+        <div className="hidden md:flex">
+          <Logo />
+        </div>
         <FormPopover align="start" side="bottom" sideOffset={18}>
           <Button
             variant="primary"
