0894b02852bbbd5b6bc9d4224a8ebc828281bf81
danny <danny.nothdurft@sugarshape.de>
2023-12-22 12:55:38 +0100

    Logo

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -6,7 +6,6 @@ import { useLocalStorage } from "usehooks-ts";
 import { useOrganization, useOrganizationList } from "@clerk/nextjs";
 
 import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Accordion } from "@/components/ui/accordion";
 
@@ -14,37 +13,32 @@ import { NavItem, Organization } from "./nav-item";
 
 interface SidebarProps {
   storageKey?: string;
-};
+}
 
-export const Sidebar = ({
-  storageKey = "t-sidebar-state",
-}: SidebarProps) => {
+export const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
   const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
     storageKey,
     {}
   );
 
-  const {
-    organization: activeOrganization,
-    isLoaded: isLoadedOrg
-  } = useOrganization();
-  const { 
-    userMemberships,
-    isLoaded: isLoadedOrgList
-  } = useOrganizationList({
+  const { organization: activeOrganization, isLoaded: isLoadedOrg } =
+    useOrganization();
+  const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
     userMemberships: {
       infinite: true,
     },
   });
 
-  const defaultAccordionValue: string[] = Object.keys(expanded)
-    .reduce((acc: string[], key: string) => {
+  const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+    (acc: string[], key: string) => {
       if (expanded[key]) {
         acc.push(key);
       }
 
       return acc;
-  }, []);
+    },
+    []
+  );
 
   const onExpand = (id: string) => {
     setExpanded((curr) => ({
@@ -72,9 +66,7 @@ export const Sidebar = ({
   return (
     <>
       <div className="font-medium text-xs flex items-center mb-1">
-        <span className="pl-4">
-          Workspaces
-        </span>
+        <span className="pl-4">Workspaces</span>
         <Button
           asChild
           type="button"
@@ -83,9 +75,7 @@ export const Sidebar = ({
           className="ml-auto"
         >
           <Link href="/select-org">
-            <Plus
-              className="h-4 w-4"
-            />
+            <Plus className="h-4 w-4" />
           </Link>
         </Button>
       </div>
